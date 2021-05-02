4e3580ea1b8b5e80b63300cd00f8602d971c0fdc
sazzadur rahman <sazzadurrahman88@gmail.com>
2021-05-02 16:25:58 +0600

    project initialization done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import DefaultBoard from "./base-board";
 function App() {
+  const [columns, setColumns] = useState(DefaultBoard.columns);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="hero is-fullheight">
+      <div className="hero-body">
+        {columns.map((column, colIndex) => (
+          <div
+            key={`${column.name}-${colIndex}`}
+            className="trello-column"
+            draggable="true"
+          >
+            <div className="title is-5 board-title">{column.name}</div>
+
+            <div className="task-container">
+              {column.tasks.map((task, taskIndex) => (
+                <div
+                  draggable="true"
+                  key={task.id}
+                  v-for="(task, taskIndex) in column.tasks"
+                  className="card row-item"
+                >
+                  <header className="card-header">
+                    <p className="card-header-title">
+                      {task.name}
+                      <span className="bin icon is-small">
+                        <i className="far fa-trash-alt"></i>
+                      </span>
+                    </p>
+                  </header>
+                  <div className="card-content" v-if="task.description !== ''">
+                    <div className="content small-text">{task.description}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function uuid() {
+  return Math.random().toString().slice(2);
+}
